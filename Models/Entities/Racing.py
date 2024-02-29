11384dd096d166d3681e7e188db853d6a2acf1c6
Juan Diego Varon Valencia <juan.varonv@opitech.com.co>
2024-02-29 18:41:31 -0500

    Se agrega comentario

--- Models/Entities/Racing.py
+++ Models/Entities/Racing.py
@@ -1,4 +1,7 @@
 class Racing:
+    '''
+    Clase que representa la carrera que se lleva a cabo.
+    '''
     def __init__(self, num_vueltas: int, name :str):
         self.num_vueltas = num_vueltas
         self.cars = []
