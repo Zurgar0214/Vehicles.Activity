e216659118f35cf7eee39869ad9e541e49a03bc5
Juan Diego Varon Valencia <juan.varonv@opitech.com.co>
2024-02-29 19:46:18 -0500

    Added fileService and load data from json files, also created main class

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-'''
-Imagina que estás creando un juego de carreras. Para ello, debes modelar diferentes tipos de vehículos, como coches, motos, camiones, etc. Cada tipo de vehículo tiene diferentes atributos y métodos.
+from Service.Loader import Loader
 
-Utiliza herencia y polimorfismo para modelar diferentes tipos de vehículos. Además, utiliza decoradores para agregar características especiales a ciertos tipos de vehículos.
 
-Finalmente, crea un programa que utilice estas clases y métodos para simular una carrera entre diferentes tipos de vehículos. Puedes agregar un sistema de puntuación y un cronómetro para hacerlo más interesante.
-
-Ayuda:
-
-Crea una clase Vehiculo que tenga los atributos básicos de un vehículo, como la velocidad máxima y la capacidad del tanque de gasolina. También debe tener métodos para acelerar, frenar
-
-Crea subclases de Vehiculo para cada tipo de vehículo que quieras modelar
-
-Utiliza polimorfismo para implementar métodos específicos de cada subclase.
-
-Utiliza decoradores para agregar características especiales a ciertos tipos de vehículos. Por ejemplo, puedes agregar un decorador llamado Turbo a la subclase Coche que aumente temporalmente la velocidad máxima del coche
-
-Debes crear un programa que utilice estas clases y métodos para simular una carrera entre diferentes tipos de vehículos. Puedes agregar un sistema de puntuación y un cronómetro para hacerlo más interesante.
-'''
+if __name__ == "__main__":
+    loader = Loader()
+    vehicles, motorcycles, cars = loader.load_data()
