fe7005bf8fe4d352481f7dfbd88e1c8b533ac1f8
Juan Diego Ramirez Muñoz <juan.1702011618@ucaldas.edu.co>
2024-02-29 20:02:48 -0500

    create welcome

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from Service.Loader import Loader
 if __name__ == "__main__":
     loader = Loader()
     vehicles, motorcycles, cars = loader.load_data()
+    print("Bienvenido al juego /n ")
+    print("Acá tienes una lista de vehículos, selecciona uno")
+    print(cars)
