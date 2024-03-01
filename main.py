664bb1f5d2d9329e2092a244b32817660eac7c92
Juan Diego Ramirez Muñoz <juan.1702011618@ucaldas.edu.co>
2024-02-29 20:58:16 -0500

    Create racing and racer

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from Service.Loader import Loader
 
 if __name__ == "__main__":
     loader = Loader()
-    vehicles = loader.load_data()
+    vehicles, racers = loader.load_data()
     print("Bienvenido a la carrera \nElige tu vehículo de la siguiente lista: \n")
     index : int = 1
     for vehicle in vehicles:
         print(f"{index}- {vehicle}")
         index += 1
     select : int =  int(input("Escribe tu elección: "))
-    print("Veiculo seleccionado: " +vehicles[select-1])
+    print(f"Vehículo seleccionado {vehicles[select-1]}")
     racer = Racer(vehicles[select-1],  input("Escribe tu nombre: "))
     racer.ChooseCar()
     select =  int(input("Cuántas vueltas quieres correr??: "))
     racing = Racing(select, "Copa Pistón")
     racing.add_player(racer)
+    racing.add_player(racers)
